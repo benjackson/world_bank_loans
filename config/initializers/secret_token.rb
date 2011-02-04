b888fd9ebfd1893dcfbdcd0e455a1d712d401519
Ben Jackson <ben.jackson1@gmail.com>
2011-02-04 09:49:21 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorldBankLoans::Application.config.secret_token = '9aa875e41be0de6c089007eaa2a90da0a1e0a9000e8cc122edc80cfe4801b20b85939b92cc3a83910ff122dee2c632a4596ebad994c862dcf10dfdf150bf35ba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WorldBankLoans::Application.config.session_store :cookie_store, :key => '_world_bank_loans_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# WorldBankLoans::Application.config.session_store :active_record_store
