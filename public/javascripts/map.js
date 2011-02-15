9d7b2ce2db0094f99fa080370bc2ab73b8200a13
Ben Jackson <ben.jackson1@gmail.com>
2011-02-15 10:00:56 +0000

    Put the map back

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -9,7 +9,7 @@ $.WorldBank.boundsChanged = function() {
 }
 
 $(document).ready(function() {
-  /*$.WorldBank.the_map = new google.maps.Map(document.getElementById("map"), {
+  $.WorldBank.the_map = new google.maps.Map(document.getElementById("map"), {
     center: new google.maps.LatLng(20, 0),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     zoom: 1,
@@ -18,7 +18,6 @@ $(document).ready(function() {
     panControl: false,
     maxZoom: 4
   });
-  */
   
   
   //google.maps.event.addListener($.WorldBank.the_map, 'bounds_changed', $.WorldBank.boundsChanged);
