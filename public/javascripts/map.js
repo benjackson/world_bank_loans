12973b034237ae8bff6059a9c06848dd4c09f95a
Ben Jackson <ben.jackson1@gmail.com>
2011-02-15 10:24:03 +0000

    Put map back

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -2,15 +2,19 @@ $.WorldBank = {}   // namespace
 
 // the google map
 $.WorldBank.the_map = null;
+$.WorldBank.map_center = new google.maps.LatLng(20, 0);
 
 $.WorldBank.boundsChanged = function() {
+  if ($.WorldBank.the_map.getZoom() == 1)
+    $.WorldBank.the_map.setCenter(map_center);
+
   // recalculate the visible markers
   $.WorldBank.Country.boundsChanged();
 }
 
 $(document).ready(function() {
   $.WorldBank.the_map = new google.maps.Map(document.getElementById("map"), {
-    center: new google.maps.LatLng(20, 0),
+    center: $.WorldBank.map_center,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     zoom: 1,
     streetViewControl: false,
@@ -20,9 +24,9 @@ $(document).ready(function() {
   });
   
   
-  //google.maps.event.addListener($.WorldBank.the_map, 'bounds_changed', $.WorldBank.boundsChanged);
+  google.maps.event.addListener($.WorldBank.the_map, 'bounds_changed', $.WorldBank.boundsChanged);
   
-  //$.get("/countries.js", null, null, "script");
+  $.get("/countries.js", null, null, "script");
   
-  //$.WorldBank.CountryInfos.start();
+  $.WorldBank.CountryInfos.start();
 });
