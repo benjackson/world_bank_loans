5f32e7c2f639a41b16ef1e53cc67c7bb57d617d4
Ben Jackson <ben.jackson1@gmail.com>
2011-02-15 08:35:21 +0000

    Removed mobile testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  has_mobile_fu(true)  # detect different mobile phones
+  has_mobile_fu  # detect different mobile phones
   
   before_filter :ask_for_a_username_and_password
   
