f45b91914d786f68bfcc0ed0a869dac505a995ca
Ben Jackson <ben.jackson1@gmail.com>
2011-02-15 09:56:53 +0000

    Testing map div on mobile

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -9,7 +9,7 @@ $.WorldBank.boundsChanged = function() {
 }
 
 $(document).ready(function() {
-  $.WorldBank.the_map = new google.maps.Map(document.getElementById("map"), {
+  /*$.WorldBank.the_map = new google.maps.Map(document.getElementById("map"), {
     center: new google.maps.LatLng(20, 0),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     zoom: 1,
@@ -18,11 +18,12 @@ $(document).ready(function() {
     panControl: false,
     maxZoom: 4
   });
+  */
   
   
-  google.maps.event.addListener($.WorldBank.the_map, 'bounds_changed', $.WorldBank.boundsChanged);
+  //google.maps.event.addListener($.WorldBank.the_map, 'bounds_changed', $.WorldBank.boundsChanged);
   
-  $.get("/countries.js", null, null, "script");
+  //$.get("/countries.js", null, null, "script");
   
-  $.WorldBank.CountryInfos.start();
+  //$.WorldBank.CountryInfos.start();
 });
