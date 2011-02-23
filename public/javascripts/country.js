4aad5001767f15b5fb1e76d119a189d0f28c1050
Ben Jackson <ben.jackson1@gmail.com>
2011-02-23 14:00:00 +0000

    Animation on the marker

--- public/javascripts/country.js
+++ public/javascripts/country.js
@@ -7,7 +7,11 @@ $.WorldBank.CountryOverlay = (function() {
       this.onAdd = function() {
         var pane = this.getPanes().floatPane;
         div = $("<div class=\"country-overlay\">" + country.getOverlayText() + "</div>");
-        $(div).click(function() { country.clicked(); });
+        $(div).click(function() {
+          country.getMarker().setAnimation(google.maps.Animation.BOUNCE);
+          setTimeout(function() { country.getMarker().setAnimation(null); }, 1000);
+          country.clicked(); 
+        });
         $(pane).append(div);
       }
       
